75d7b6d63dbd6cf08156474336ea38702eda8396
Fabian Canas <fabian@mojotech.com>
2014-09-03 16:47:20 -0400

    Dynamic UIFont properties on TRIMTheme subclasses.

--- Example/Tests/TRIMSubclassing.m
+++ Example/Tests/TRIMSubclassing.m
@@ -29,12 +29,18 @@
 
 - (void)testColors
 {
-    XCTAssertEqualObjects([self.theme backgroundColor], [UIColor redColor], @"Background color should be red.");
+    XCTAssertEqualObjects([self.theme red], [UIColor redColor], @"Background color should be red.");
     //#28ba00
     XCTAssertEqualObjects([self.theme mojoGreen],
                           [UIColor colorWithRed:0x28/255. green:0xba/255. blue:0 alpha:1.0], @"Mojo green should be mojo green.");
     XCTAssertEqualObjects([self.theme colorForKey:@"primaryBrandColor"],
                           [UIColor colorWithRed:0x28/255. green:0xba/255. blue:0 alpha:1.0], @"Primary brand color should be mojo green. - Hierarchical color names.");
+}
+
+- (void)testCustomFontLoading
+{
+    XCTAssertEqualObjects([self.theme goofyFont],
+                          [UIFont fontWithName:@"Zapfino" size:27], @"Should load and build the right font with a name and size key");
 }
 
 @end
